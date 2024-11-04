0a7f5c34b46904ff0ca210dad4a856b0a1ef8bf4
embbov <embbov@gmail.com>
2024-11-04 01:00:28 +0100

    Add negate method for literals

--- src/satsolver.py
+++ src/satsolver.py
@@ -3,3 +3,7 @@ from enum import Enum
 class Satisfiability(Enum):
     SATISFIABLE = "satisfiable"
     UNSATISFIABLE = "unsatisfiable"
+
+def negate(literal):
+    """Returns the negation of literal."""
+    return -literal
