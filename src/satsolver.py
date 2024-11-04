3e7313bbed7e0946912886c849ef458ef365451a
embbov <embbov@gmail.com>
2024-11-04 01:18:59 +0100

    Add methods unit_propagate, prove, main

--- src/satsolver.py
+++ src/satsolver.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import sys
 
 class Satisfiability(Enum):
     SATISFIABLE = "satisfiable"
@@ -7,3 +8,23 @@ class Satisfiability(Enum):
 def negate(literal):
     """Returns the negation of literal."""
     return -literal
+
+def unit_propagate(formula, literal):
+    """Performs unit subsumption or unit resolution if applicable."""
+    return
+
+def prove(sequent):
+    return
+
+def main():
+    if len(sys.argv) == 1:
+        print("No argument provided. Exiting program.")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        prove(sys.argv[1])
+    else:
+        print("Too many arguments provided. Exiting program.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
